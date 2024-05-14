96a6099e8ee74fca7e087c033a19256a93b739bd
Edwin Ruiz <eruiz1545@gmail.com>
2024-05-14 18:11:44 -0500

    R9

--- src/main/java/environment/service/EnvironmentService.java
+++ src/main/java/environment/service/EnvironmentService.java
@@ -10,4 +10,6 @@ public interface EnvironmentService {
     List<Video> findAllVideos() throws VideoNotFoundException;
     Video add(Video video);
     View add(View view);
+    List<Video> find(String title) throws VideoNotFoundException;
+    List<Video> find(Double fromDuration, Double toDuration) throws VideoNotFoundException;
 }
